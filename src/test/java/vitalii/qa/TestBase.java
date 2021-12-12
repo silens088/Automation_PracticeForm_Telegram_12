1a3ebd29987c8a9e1ef05b8974d9f9dcd1c4b061
Vitalii_Velich <silens088@gmail.com>
2021-12-12 20:21:07 +0300

    add Attach

--- src/test/java/vitalii/qa/TestBase.java
+++ src/test/java/vitalii/qa/TestBase.java
@@ -49,6 +49,6 @@ public class TestBase{
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
+        //Attach.addVideo();
     }
 }
