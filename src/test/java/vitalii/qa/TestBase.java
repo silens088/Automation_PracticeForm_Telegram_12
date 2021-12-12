cf97e5204b135e65d75b5e6f88e5856b98a744ff
Vitalii_Velich <silens088@gmail.com>
2021-12-12 19:51:41 +0300

    add Configuration.remote

--- src/test/java/vitalii/qa/TestBase.java
+++ src/test/java/vitalii/qa/TestBase.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 import java.util.Locale;
@@ -20,12 +21,23 @@ public class TestBase{
     String randomTelephone = faker.number().digits(10);
 
 
+
+
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()); //добавили лисенер для аллюр
         Configuration.startMaximized = true;
         //Configuration.startMaximized = false;
         //Configuration.browserSize = "1366x768";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/"; //для удаленного запуска тестов на селениде - ресурс школы
+
+        //конфигурация селеноида
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 
 }
