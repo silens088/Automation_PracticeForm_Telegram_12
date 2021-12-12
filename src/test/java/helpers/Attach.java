9dd673a693bb7b5c77a658c2e1a97a58a5903fe3
Vitalii_Velich <silens088@gmail.com>
2021-12-12 20:46:39 +0300

    added addVideo

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -41,25 +41,26 @@ public class Attach {
         );
     }
 
-//    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-//    public static String addVideo() {
-//        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-//                + getVideoUrl(getSessionId())
-//                + "' type='video/mp4'></video></body></html>";
-//    }
-//
-//    public static URL getVideoUrl(String sessionId) {
-//        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
-//
-//        try {
-//            return new URL(videoUrl);
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    public static String getSessionId(){
-//        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
-//    }
+    //добавляем видео в отчет (небольшой медиа плеер)
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl(getSessionId())
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+    public static URL getVideoUrl(String sessionId) {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
+
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
 }
