7f1291c070d1ecb90e3d74fde6eb228f6ee8115a
Vitalii_Velich <silens088@gmail.com>
2021-12-12 18:35:17 +0300

    deleted test

--- src/test/java/vitalii/qa/PracticeFormTests.java
+++ src/test/java/vitalii/qa/PracticeFormTests.java
@@ -1,10 +1,8 @@
 package vitalii.qa;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static vitalii.qa.TestData.randomEmail;
 
 public class PracticeFormTests extends TestBase {
