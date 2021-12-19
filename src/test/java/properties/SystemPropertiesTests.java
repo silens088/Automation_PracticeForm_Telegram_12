69f1559e901379b4902dddda089d0c61af33a645
Vitalii_Velich <silens088@gmail.com>
2021-12-19 22:15:33 +0300

    added someTest5

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -16,7 +16,7 @@ public class SystemPropertiesTests {
 
     @Test
     void someTest1() {
-        String value = System.getProperty("value","default_value");  //если value нет, забираем дефолтное значение.
+        String value = System.getProperty("value", "default_value");  //если value нет, забираем дефолтное значение.
         System.out.println(value);
         //default_value
     }
@@ -32,7 +32,7 @@ public class SystemPropertiesTests {
     @Test
     void someTest3() {
         System.setProperty("value", "another_value");
-        String value = System.getProperty("value","default_value");
+        String value = System.getProperty("value", "default_value");
         System.out.println(value);
         //another_value
     }
@@ -41,10 +41,25 @@ public class SystemPropertiesTests {
     @Test
     @Tag("properties")
     void someTest4() {
-        String value = System.getProperty("browse","chrome");
+        String value = System.getProperty("browse", "chrome");
         System.out.println(value);
-        //gradle clean properties_tests - но поч запуск через консоль не работает ???
+        //gradle clean properties_tests
         //chrome
+        //  gradle clean properties_tests -Dbrowser=opera
+        //  opera
     }
 
+    @Test
+    @Tag("properties")
+    void someTest5() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+
+        // gradle clean properties_tests -Dbrowser=opera -Dversion=93
+    }
 }
