cd4c9dafee3c959e3c9da3d7b91c3bce8e04f697
Vitalii_Velich <silens088@gmail.com>
2022-01-02 17:09:44 +0300

    added Owner task tag

--- src/test/java/properties/OwnerLibTests.java
+++ src/test/java/properties/OwnerLibTests.java
@@ -2,6 +2,7 @@ package properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
@@ -9,6 +10,7 @@ import static java.lang.String.format;
 
 //здесь мы с помощью либы owner скрываем данные. (логин пароль)
 
+@Tag("properties") //тэг для запуска таски в дженкенс
 public class OwnerLibTests {
     public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
